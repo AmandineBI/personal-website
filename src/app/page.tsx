fffe48dd0efde11698b1e3f86f939b86e240266c
Amandine <83656052+AmandineScopely@users.noreply.github.com>
2026-04-18 13:43:10 +0200

    Add new homepage

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { StarsBlock } from "@/blocks/Stars/Component";
+
+export default function HomePage() {
+  return (
+    <main>
+      <StarsBlock title="Welcome to my site" />
+    </main>
+  )
+}
